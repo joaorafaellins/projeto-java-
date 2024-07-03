26e876b0b22bac65c2a893a3854313c49c31f604
joaorafaellins <168040651+joaorafaellins@users.noreply.github.com>
2024-07-03 16:53:48 -0300

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Operador de atribuição: =");
+        String texto = "João";//declaração da variavel "texto", do tipo string cujo valor recebido é João
+        System.out.println("String texto = \"João\";");
+        System.out.println(texto);
+
+        System.out.println("Operador de soma: +");
+        int soma = 1 + 1;// declaração da variavel "soma", do tipo inteiro 
+        System.out.println("int soma = 1 + 1;");
+        System.out.println(soma);
+
+        System.out.println("Operador de subtração: -");
+        int subtracao = 1 - 1;// declaração da variavel "subtracao", do tipo inteiro
+        System.out.println("int subtracao = 1 - 1;");
+        System.out.println(subtracao);
+
+        System.out.println("Operador de divisão: /");
+        int divisao = 1 / 1;// declaração da variavel "divisao",do tipo inteiro
+        System.out.println("int divisao = 1 / 1;");
+        System.out.println(divisao);
+
+        System.out.println("Operador de multiplicação: *");
+        int multiplicacao = 1 * 1;//declaração da variavel"multiplicação", do tipo inteiro
+        System.out.println("int multiplicacao = 1 * 1;");
+        System.out.println(multiplicacao);
+
+        System.out.println("Operador de concatenação de strings: +");
+        String concatenacao = "Texto 1 + " + "Texto 2";//declaração da variavel"concatenacao", do tipo String
+        System.out.println("String concatenacao = \"Texto 1 + \" + \"Texto 2\";");
+        System.out.println(concatenacao);
+    }
+}
