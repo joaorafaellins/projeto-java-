216a6f93b05a586120c0b66a1212449efb648a74
joaorafaellins <168040651+joaorafaellins@users.noreply.github.com>
2024-07-04 15:43:51 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,21 @@
+public class Variaveis{
+
+public static void main(String[] args) throws Exception{
+    int varInt =2;//declaração da variavel varInt do tipo int,com valor recebido 2
+    System.out.println("Varieavel do tipo int(inteiro):"+varInt);
+    Integer varInteger =3;//declaração da variavel varInteger do tipo Int, com valor recebido 3
+    System.out.println("Varieavel do tipo Integer(inteiro):"+varInteger);
+    String varString ="texto";//declaração da variavel varString, com valor recebido texto
+    System.out.println("Varieavel do tipo String(texto):"+varString);
+    boolean varBoolean = true;//declaração da variavel varBoolean do tipo boolean, com valor recebido true
+    System.out.println("Varieavel do tipo boolean (true,false):"+varBoolean);
+    byte varByte =1;//declaração da variavel varByte do tipo byte, com valor recebido 1
+    System.out.println("Varieavel do tipo byte(dados binarios):"+varByte);
+    char varChar ='r';//declaração da variavel varChar do tipo char, com valor recebido r
+    System.out.println("Varieavel do tipo char:"+varChar);
+    float varFloat =19.99f;//declaração da variavel varFloat do tipo float,com valor recebido 19.99f
+    System.out.println("Varieavel do tipo float:"+varFloat);
+    
+
+
+}}
